4402666cbb9062531196eb2709d24829e59ab7bc
D.A Jalloh <info@codewithjalloh.com>
2016-07-26 20:53:27 +0100

    myTableViewCell added

---
+++ myTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  myTableViewCell.swift
+//  CoreDataWithTable
+//
+//  Created by wealthyjalloh on 26/07/2016.
+//  Copyright © 2016 CWJ. All rights reserved.
+//
+
+import UIKit
+
+class myTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
