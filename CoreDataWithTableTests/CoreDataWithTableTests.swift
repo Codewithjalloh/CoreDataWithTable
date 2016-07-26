a4927079b979b8a89b9032a71e1f5724d1fa13a3
D.A Jalloh <info@codewithjalloh.com>
2016-07-26 20:48:21 +0100

    Initial Commit

---
+++ CoreDataWithTableTests/CoreDataWithTableTests.swift
@@ -0,0 +1,36 @@
+//
+//  CoreDataWithTableTests.swift
+//  CoreDataWithTableTests
+//
+//  Created by wealthyjalloh on 26/07/2016.
+//  Copyright © 2016 CWJ. All rights reserved.
+//
+
+import XCTest
+@testable import CoreDataWithTable
+
+class CoreDataWithTableTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
